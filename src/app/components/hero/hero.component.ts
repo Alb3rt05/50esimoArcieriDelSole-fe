14c6502dab0efa9829c8fc605fd488aeba293359
Alby <alberto.cosyanzo05@gmail.com>
2026-02-01 19:03:34 +0100

    FIX:Fixed mobile responsive

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-hero',
-    imports: [],
-    templateUrl: './hero.component.html',
-    styleUrls: ['./hero.component.scss']
+  selector: 'app-hero',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit, OnDestroy {
+
+  backgroundImages: string[] = [
+    'assets/images/hero-1.jpg',
+    'assets/images/hero-2.jpg',
+    'assets/images/hero-3.jpg'
+    // Add more paths here
+  ];
+
+  currentImageIndex = 0;
+  private intervalId: any;
+
+  ngOnInit() {
+    this.startSlideshow();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startSlideshow() {
+    this.intervalId = setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.backgroundImages.length;
+    }, 2000); // Change every 2 seconds
+  }
+
   scrollTo(id: string) {
     const element = document.getElementById(id);
     if (element) {
